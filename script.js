6f9f3d8fed1c92bfe4b7ef8353347c19a76088a1
Ireese <ireese@linz.govt.nz>
2021-04-14 15:12:00 +1200

    inital commit

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+Procedural.init( {
+    container: document.getElementById( 'container' ),
+    // For further details see:
+    // github.com/felixpalmer/procedural-gl-js/wiki/Data-sources
+    datasource: {
+      elevation: {
+        pixelEncoding: 'terrain-rgb', // or 'nasadem', 'terrarium'
+        pixelFormat: 'terrain-rgb',
+        urlFormat: 'http://localhost:8000/tiles/{z}/{x}/{y}.png',
+        attribution: 'Elevation attribution'
+      },
+      imagery: {
+        urlFormat: 'https://tiles.maps.linz.io/nz_colour_basemap/GLOBAL_MERCATOR/{z}/{x}/{y}.png',
+        attribution: 'Imagery attribution'
+      },
+    }
+  } );
+
+Procedural.displayLocation( { latitude: -46.061612354797795, longitude:  167.13911036209228} );
+
+
